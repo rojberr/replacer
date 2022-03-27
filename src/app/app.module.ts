cd7a346226146bee6bffc06b2c88c15637a0a8f6
Rojberr <rojberr@outlook.com>
2022-03-27 19:00:09 +0200

    Init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SubscriberComponent } from './subscriber/subscriber.component';
+import { TemplateTodoComponent } from './_template/template-todo/template-todo.component';
+import { TemplateTodoFormComponent } from './_template/template-todo-form/template-todo-form.component';
+import { TemplateHeaderComponent } from './_template/template-header/template-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubscriberComponent,
+    TemplateTodoComponent,
+    TemplateTodoFormComponent,
+    TemplateHeaderComponent
   ],
   imports: [
     BrowserModule
